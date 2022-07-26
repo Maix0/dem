1deb8d7b1f1498c5d457ea14f67704863465cc31
Maix0 <maix522@gmail.com>
2022-07-26 23:34:19 +0200

    updated dem-http generation

--- generate_dem_http.sh
+++ generate_dem_http.sh
@@ -1,1 +1,4 @@
 java -jar openapi-generator-cli.jar generate -i http://localhost:8000/api/openapi.json -g rust -o dem-http --additional-properties=packageName=dem-http
+echo "Applying Cargo.toml patch"
+git apply http.patch
+echo "Finished generating dem-http"
